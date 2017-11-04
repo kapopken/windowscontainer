d172169023491dd1a8a554a5bd26d9546bc379ae
Kevin Popken <kapopken@msn.com>
2017-11-04 07:48:18 -0500

    Initial push

---
+++ source/WindowsContainer/App/DBTodo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsContainer.App
+{
+    public class DBTodo : DbContext
+    {
+        public DBTodo(DbContextOptions<DBTodo> options) : base(options)
+        {
+        }
+        public DbSet<Todo> Todo { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=tcp:127.0.0.1,5433;Initial Catalog=TodoDB;User Id=sa;Password=Pass@word");
+        //}
+    }
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
